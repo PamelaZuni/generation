685baba22611cba7ee9d68fd30b9f17f20234e39
Pamela Zuni <94977548+PamelaZuni@users.noreply.github.com>
2024-03-21 00:20:17 +0100

    Update ex4.js

--- exercise4/ex4.js
+++ exercise4/ex4.js
@@ -17,7 +17,7 @@ n4 = read.questionFloat('\nDigit the number 4 : ');
 
 result = (n1 * n2) - (n3 * n4);
 
-console.log(`The difference between number 1 and 2 to number 3 and 4 is ${result}`)
+console.log(`The difference between number 1 and 2 to number 3 and 4 is ${result}`);
 
 //The differnce between number 1 and 2 to number 3 and 4 is -26
 
